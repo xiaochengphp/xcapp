f69f5f084376e0ef85e9ab2921890029973d8306
xiaochengphp <1427514105@qq.com>
2018-03-04 12:47:18 +0800

    Add files via upload

---
+++ data/che-data.js
@@ -0,0 +1,696 @@
+var cheData = [{
+    'name': '奥迪',
+    'index': 'A',
+    'icon': '/images/che/99_90_20140314021327910.jpg'
+}, {
+    'name': '阿尔法·罗密欧',
+    'index': 'A',
+    'icon': '/images/che/99_90_2007106113711674.jpg'
+}, {
+    'name': 'ALPINA',
+    'index': 'A',
+    'icon': '/images/che/99_90_20161102025501466.jpg'
+}, {
+    'name': '阿斯顿·马丁',
+    'index': 'A',
+    'icon': '/images/che/99_90_2007106113228599.jpg'
+}, {
+    'name': '比速',
+    'index': 'B',
+    'icon': '/images/che/99_90_20161026022613607.jpg'
+}, {
+    'name': '北京汽车',
+    'index': 'B',
+    'icon': '/images/che/99_90_20140106100523179.png'
+}, {
+    'name': '北汽威旺',
+    'index': 'B',
+    'icon': '/images/che/99_90_20140106103659161.jpg'
+}, {
+    'name': '北汽幻速',
+    'index': 'B',
+    'icon': '/images/che/99_90_20140403102843505.png'
+}, {
+    'name': '巴博斯',
+    'index': 'B',
+    'icon': '/images/che/99_90_20140815105349131.jpg'
+}, {
+    'name': '北汽新能源',
+    'index': 'B',
+    'icon': '/images/che/99_90_20141217104427907.jpg'
+}, {
+    'name': '宝沃',
+    'index': 'B',
+    'icon': '/images/che/99_90_20150917052648178.jpg'
+}, {
+    'name': '奔腾',
+    'index': 'B',
+    'icon': '/images/che/99_90_20160923044234670.jpg'
+}, {
+    'name': '本田',
+    'index': 'B',
+    'icon': '/images/che/99_90_200710612126507.jpg'
+}, {
+    'name': '宾利',
+    'index': 'B',
+    'icon': '/images/che/99_90_2007106135441171.gif'
+}, {
+    'name': '奔驰',
+    'index': 'B',
+    'icon': '/images/che/99_90_20130516034604229.jpg'
+}, {
+    'name': '宝马',
+    'index': 'B',
+    'icon': '/images/che/99_90_20130516034314884.jpg'
+}, {
+    'name': '标致',
+    'index': 'B',
+    'icon': '/images/che/99_90_20071061321816.jpg'
+}, {
+    'name': '布加迪',
+    'index': 'B',
+    'icon': '/images/che/99_90_2007106112930312.jpg'
+}, {
+    'name': '保时捷',
+    'index': 'B',
+    'icon': '/images/che/99_90_2007106135311183.jpg'
+}, {
+    'name': '别克',
+    'index': 'B',
+    'icon': '/images/che/99_90_2007106114528906.jpg'
+}, {
+    'name': '北汽制造',
+    'index': 'B',
+    'icon': '/images/che/99_90_2007106114528906.jpg'
+}, {
+    'name': '比亚迪',
+    'index': 'B',
+    'icon': '/images/che/99_90_20091111163529182.jpg'
+}, {
+    'name': '宝骏',
+    'index': 'B',
+    'icon': '/images/che/99_90_20111103025151854.jpg'
+}, {
+    'name': '成功',
+    'index': 'C',
+    'icon': '/images/che/99_90_20160906053801726.jpg'
+}, {
+    'name': '长安欧尚',
+    'index': 'C',
+    'icon': '/images/che/99_90_20180108034519150.jpg'
+}, {
+    'name': '长安跨越',
+    'index': 'C',
+    'icon': '/images/che/99_90_20180108032347744.jpg'
+}, {
+    'name': '长安轻型车',
+    'index': 'C',
+    'icon': '/images/che/99_90_20180108033227600.jpg'
+}, {
+    'name': '昌河',
+    'index': 'C',
+    'icon': '/images/che/99_90_20091030154942642.jpg'
+}, {
+    'name': '长城',
+    'index': 'C',
+    'icon': '/images/che/99_90_20091112155915830.jpg'
+}, {
+    'name': '长安',
+    'index': 'C',
+    'icon': '/images/che/99_90_20120224034258747.jpg'
+}, {
+    'name': '传祺',
+    'index': 'C',
+    'icon': '/images/che/99_90_20107814559779.jpg'
+}, {
+    'name': 'DS',
+    'index': 'D',
+    'icon': '/images/che/99_90_20140106034821320.jpg'
+}, {
+    'name': '东风风神',
+    'index': 'D',
+    'icon': '/images/che/99_90_20140114025100793.jpg'
+}, {
+    'name': '东风风度',
+    'index': 'D',
+    'icon': '/images/che/99_90_20140114031209919.png'
+}, {
+    'name': '东风风行',
+    'index': 'D',
+    'icon': '/images/che/99_90_20140114034653431.png'
+}, {
+    'name': '东风小康',
+    'index': 'D',
+    'icon': '/images/che/99_90_20140114040307317.jpg'
+}, {
+    'name': '大众',
+    'index': 'D',
+    'icon': '/images/che/99_90_2007106114920951.jpg'
+}, {
+    'name': '道奇',
+    'index': 'D',
+    'icon': '/images/che/99_90_2007106112524539.jpg'
+}, {
+    'name': '东南',
+    'index': 'D',
+    'icon': '/images/che/99_90_20091112142011134.jpg'
+}, {
+    'name': '大迪',
+    'index': 'D',
+    'icon': '/images/che/99_90_201051393559674.jpg'
+}, {
+    'name': '东风',
+    'index': 'D',
+    'icon': '/images/che/99_90_20140114043342468.jpg'
+}, {
+    'name': '大宇',
+    'index': 'D',
+    'icon': '/images/che/99_90_20101021121223611.jpg'
+}, {
+    'name': '大通',
+    'index': 'D',
+    'icon': '/images/che/99_90_20110930111003846.jpg'
+}, {
+    'name': '福汽启腾',
+    'index': 'F',
+    'icon': '/images/che/99_90_20160906051125697.png'
+}, {
+    'name': '丰田',
+    'index': 'F',
+    'icon': '/images/che/99_90_20140318040243723.jpg'
+}, {
+    'name': '菲亚特',
+    'index': 'F',
+    'icon': '/images/che/99_90_20131114103409381.jpg'
+}, {
+    'name': '法拉利',
+    'index': 'F',
+    'icon': '/images/che/99_90_2007106134942406.jpg'
+}, {
+    'name': '福特',
+    'index': 'F',
+    'icon': '/images/che/99_90_2007106122429801.jpg'
+}, {
+    'name': '福迪',
+    'index': 'F',
+    'icon': '/images/che/99_90_201022594113517.jpg'
+}, {
+    'name': '福田',
+    'index': 'F',
+    'icon': '/images/che/99_90_20140217093625782.jpg'
+}, {
+    'name': '观致',
+    'index': 'G',
+    'icon': '/images/che/99_90_20130522033705654.jpg'
+}, {
+    'name': 'GMC',
+    'index': 'G',
+    'icon': '/images/che/99_90_2007106151024504.jpg'
+}, {
+    'name': '华骐',
+    'index': 'H',
+    'icon': '/images/che/99_90_20171017043322182.jpg'
+}, {
+    'name': '哈弗',
+    'index': 'H',
+    'icon': '/images/che/99_90_20131224023955432.jpg'
+}, {
+    'name': '华颂',
+    'index': 'H',
+    'icon': '/images/che/99_90_20150915050648442.jpg'
+}, {
+    'name': '海格',
+    'index': 'H',
+    'icon': '/images/che/99_90_20160906040850677.gif'
+}, {
+    'name': '恒天',
+    'index': 'H',
+    'icon': '/images/che/99_90_20160906042216860.jpg'
+}, {
+    'name': '华凯',
+    'index': 'H',
+    'icon': '/images/che/99_90_20160906054346594.jpg'
+}, {
+    'name': '汉腾',
+    'index': 'H',
+    'icon': '/images/che/99_90_20160906054831679.jpg'
+}, {
+    'name': '华泰新能源',
+    'index': 'H',
+    'icon': '/images/che/99_90_20160923044610154.jpg'
+}, {
+    'name': '红旗',
+    'index': 'H',
+    'icon': '/images/che/99_90_20091111154713403.jpg'
+}, {
+    'name': '华普',
+    'index': 'H',
+    'icon': '/images/che/99_90_20091112105152390.jpg'
+}, {
+    'name': '哈飞',
+    'index': 'H',
+    'icon': '/images/che/99_90_2007105203933220.jpg'
+}, {
+    'name': '海马',
+    'index': 'H',
+    'icon': '/images/che/99_90_20091112144523631.jpg'
+}, {
+    'name': '汇众',
+    'index': 'H',
+    'icon': '/images/che/99_90_2007105225938370.jpg'
+}, {
+    'name': '黄海',
+    'index': 'H',
+    'icon': '/images/che/99_90_200910271603528.jpg'
+}, {
+    'name': '华泰',
+    'index': 'H',
+    'icon': '/images/che/99_90_20120209023958430.jpg'
+}, {
+    'name': '悍马',
+    'index': 'H',
+    'icon': '/images/che/99_90_2010102112837892.jpg'
+}, {
+    'name': '君马',
+    'index': 'J',
+    'icon': '/images/che/99_90_20171122032042315.jpg'
+}, {
+    'name': '金旅',
+    'index': 'J',
+    'icon': '/images/che/99_90_20160906055346232.jpg'
+}, {
+    'name': '江铃集团轻汽',
+    'index': 'J',
+    'icon': '/images/che/99_90_20160906052354464.jpg'
+}, {
+    'name': '江铃集团新能源',
+    'index': 'J',
+    'icon': '/images/che/99_90_20161027105108153.jpg'
+}, {
+    'name': '捷豹',
+    'index': 'J',
+    'icon': '/images/che/99_90_2007106132425403.jpg'
+}, {
+    'name': '吉普',
+    'index': 'J',
+    'icon': '/images/che/99_90_2007106114654604.jpg'
+}, {
+    'name': '吉利汽车',
+    'index': 'J',
+    'icon': '/images/che/99_90_20161201050753101.jpg'
+}, {
+    'name': '吉奥',
+    'index': 'J',
+    'icon': '/images/che/99_90_2009111215319303.jpg'
+}, {
+    'name': '江铃',
+    'index': 'J',
+    'icon': '/images/che/99_90_2007105212137179.jpg'
+}, {
+    'name': '九龙',
+    'index': 'J',
+    'icon': '/images/che/99_90_20098171549713.jpg'
+}, {
+    'name': '江淮',
+    'index': 'J',
+    'icon': '/images/che/99_90_2009111310200255.jpg'
+}, {
+    'name': '金杯',
+    'index': 'J',
+    'icon': '/images/che/99_90_20091113102613705.jpg'
+}, {
+    'name': '金龙',
+    'index': 'J',
+    'icon': '/images/che/99_90_20140217092621843.jpg'
+}, {
+    'name': '卡威',
+    'index': 'K',
+    'icon': '/images/che/99_90_20140313102242230.jpg'
+}, {
+    'name': 'KTM',
+    'index': 'K',
+    'icon': '/images/che/99_90_20140303100338490.jpg'
+}, {
+    'name': '凯翼',
+    'index': 'K',
+    'icon': '/images/che/99_90_20141202101858283.jpg'
+}, {
+    'name': '康迪',
+    'index': 'K',
+    'icon': '/images/che/99_90_20150512045605916.jpg'
+}, {
+    'name': '德国卡尔森',
+    'index': 'K',
+    'icon': '/images/che/99_90_20161104104912743.jpg'
+}, {
+    'name': '柯尼赛格',
+    'index': 'K',
+    'icon': '/images/che/99_90_200921145535397.jpg'
+}, {
+    'name': '凯迪拉克',
+    'index': 'K',
+    'icon': '/images/che/99_90_20171222021146455.jpg'
+}, {
+    'name': '克莱斯勒',
+    'index': 'K',
+    'icon': '/images/che/99_90_20071061318338.jpg'
+}, {
+    'name': '开瑞',
+    'index': 'K',
+    'icon': '/images/che/99_90_20094219453346.jpg'
+}, {
+    'name': '领克',
+    'index': 'L',
+    'icon': '/images/che/99_90_20171120112934815.png'
+}, {
+    'name': '陆地方舟',
+    'index': 'L',
+    'icon': '/images/che/99_90_20140519094535403.jpg'
+}, {
+    'name': '雷克萨斯',
+    'index': 'L',
+    'icon': '/images/che/99_90_200712610119182.jpg'
+}, {
+    'name': '铃木',
+    'index': 'L',
+    'icon': '/images/che/99_90_2007106125957739.jpg'
+}, {
+    'name': '劳斯莱斯',
+    'index': 'L',
+    'icon': '/images/che/99_90_2007106132012795.jpg'
+}, {
+    'name': '雷诺',
+    'index': 'L',
+    'icon': '/images/che/99_90_20150416095455374.jpg'
+}, {
+    'name': '兰博基尼',
+    'index': 'L',
+    'icon': '/images/che/99_90_2007106131034746.jpg'
+}, {
+    'name': '青年莲花',
+    'index': 'L',
+    'icon': '/images/che/99_90_20130628092257670.jpg'
+}, {
+    'name': '林肯',
+    'index': 'L',
+    'icon': '/images/che/99_90_20061130184745178.gif'
+}, {
+    'name': '力帆',
+    'index': 'L',
+    'icon': '/images/che/99_90_2006811763973832.gif'
+}, {
+    'name': '陆风',
+    'index': 'L',
+    'icon': '/images/che/99_90_20068115561017687.gif'
+}, {
+    'name': '猎豹',
+    'index': 'L',
+    'icon': '/images/che/99_90_20170609033537502.jpg'
+}, {
+    'name': '理念',
+    'index': 'L',
+    'icon': '/images/che/99_90_2011516171357186.jpg'
+}, {
+    'name': '路特斯',
+    'index': 'L',
+    'icon': '/images/che/99_90_20130628092346725.jpg'
+}, {
+    'name': '摩根',
+    'index': 'M',
+    'icon': '/images/che/99_90_20130621094405479.jpg'
+}, {
+    'name': '迈凯伦',
+    'index': 'M',
+    'icon': '/images/che/99_90_20130709111148529.jpg'
+}, {
+    'name': '马自达',
+    'index': 'M',
+    'icon': '/images/che/99_90_200710613439181.jpg'
+}, {
+    'name': '迈巴赫',
+    'index': 'M',
+    'icon': '/images/che/99_90_20071061372251.jpg'
+}, {
+    'name': '迷你',
+    'index': 'M',
+    'icon': '/images/che/99_90_200961011121976.jpg'
+}, {
+    'name': '玛莎拉蒂',
+    'index': 'M',
+    'icon': '/images/che/99_90_2007106131527564.jpg'
+}, {
+    'name': '曼',
+    'index': 'M',
+    'icon': '/images/che/99_90_200710614264409.jpg'
+}, {
+    'name': '迈克',
+    'index': 'M',
+    'icon': '/images/che/99_90_2007106141513246.gif'
+}, {
+    'name': 'MG',
+    'index': 'M',
+    'icon': '/images/che/99_90_20130411041645709.jpg'
+}, {
+    'name': 'Noble',
+    'index': 'N',
+    'icon': '/images/che/99_90_20130809103809527.jpg'
+}, {
+    'name': '南京金龙',
+    'index': 'N',
+    'icon': '/images/che/99_90_20160906051850636.jpg'
+}, {
+    'name': '南汽',
+    'index': 'N',
+    'icon': '/images/che/99_90_2010102112205469.jpg'
+}, {
+    'name': '纳智捷',
+    'index': 'N',
+    'icon': '/images/che/99_90_201151794624270.jpg'
+}, {
+    'name': '欧朗汽车',
+    'index': 'O',
+    'icon': '/images/che/99_90_20160906040259414.jpg'
+}, {
+    'name': '讴歌',
+    'index': 'O',
+    'icon': '/images/che/99_90_20071061231761.jpg'
+}, {
+    'name': '欧宝',
+    'index': 'O',
+    'icon': '/images/che/99_90_2007106124047939.jpg'
+}, {
+    'name': '帕加尼',
+    'index': 'P',
+    'icon': '/images/che/99_90_200912012554663.jpg'
+}, {
+    'name': '前途汽车',
+    'index': 'Q',
+    'icon': '/images/che/99_90_20161028114246322.jpg'
+}, {
+    'name': '起亚',
+    'index': 'Q',
+    'icon': '/images/che/99_90_200710612572466.jpg'
+}, {
+    'name': '奇瑞',
+    'index': 'Q',
+    'icon': '/images/che/99_90_20130812105333922.jpg'
+}, {
+    'name': '启辰',
+    'index': 'Q',
+    'icon': '/images/che/99_90_2010102112939194.jpg'
+}, {
+    'name': '如虎',
+    'index': 'R',
+    'icon': '/images/che/99_90_20160906050204381.jpg'
+}, {
+    'name': 'Rezvani',
+    'index': 'R',
+    'icon': '/images/che/99_90_20161208052316638.jpg'
+}, {
+    'name': 'Rimac',
+    'index': 'R',
+    'icon': '/images/che/99_90_20161209041833260.jpg'
+}, {
+    'name': '日产',
+    'index': 'R',
+    'icon': '/images/che/99_90_200710612511456.jpg'
+}, {
+    'name': '瑞麒',
+    'index': 'R',
+    'icon': '/images/che/99_90_2009421101258733.jpg'
+}, {
+    'name': '荣威',
+    'index': 'R',
+    'icon': '/images/che/99_90_2009111214277730.jpg'
+}, {
+    'name': '斯达泰克',
+    'index': 'S',
+    'icon': '/images/che/99_90_20150605092947435.png'
+}, {
+    'name': '赛麟',
+    'index': 'S',
+    'icon': '/images/che/99_90_20151211021404353.jpg'
+}, {
+    'name': '斯威汽车',
+    'index': 'S',
+    'icon': '/images/che/99_90_20161104090523331.jpg'
+}, {
+    'name': '塞恩Scion',
+    'index': 'S',
+    'icon': '/images/che/99_90_20161122043606375.jpg'
+}, {
+    'name': '西尔贝SSC',
+    'index': 'S',
+    'icon': '/images/che/99_90_20161226032235722.jpg'
+}, {
+    'name': '三菱',
+    'index': 'S',
+    'icon': '/images/che/99_90_2007106121015675.jpg'
+}, {
+    'name': '斯巴鲁',
+    'index': 'S',
+    'icon': '/images/che/99_90_20071061332292.jpg'
+}, {
+    'name': '双龙',
+    'index': 'S',
+    'icon': '/images/che/99_90_200710613303198.jpg'
+}, {
+    'name': '世爵',
+    'index': 'S',
+    'icon': '/images/che/99_90_2009211515949.jpg'
+}, {
+    'name': '斯柯达',
+    'index': 'S',
+    'icon': '/images/che/99_90_20130715104505118.jpg'
+}, {
+    'name': '双环',
+    'index': 'S',
+    'icon': '/images/che/99_90_2007105214512529.jpg'
+}, {
+    'name': '上海',
+    'index': 'S',
+    'icon': '/images/che/99_90_201082710125473.jpg'
+}, {
+    'name': '特斯拉',
+    'index': 'T',
+    'icon': '/images/che/99_90_20140226043937789.jpg'
+}, {
+    'name': '泰卡特',
+    'index': 'T',
+    'icon': '/images/che/99_90_20140227032255743.jpg'
+}, {
+    'name': '腾势',
+    'index': 'T',
+    'icon': '/images/che/99_90_20140429094326911.jpg'
+}, {
+    'name': '潍柴英致',
+    'index': 'W',
+    'icon': '/images/che/99_90_20150901113333350.jpg'
+}, {
+    'name': '魏派WEY',
+    'index': 'W',
+    'icon': '/images/che/99_90_20161122102002281.jpg'
+}, {
+    'name': '蔚来',
+    'index': 'W',
+    'icon': '/images/che/99_90_20161123042435154.jpg'
+}, {
+    'name': '五十铃',
+    'index': 'W',
+    'icon': '/images/che/99_90_200710615213908.gif'
+}, {
+    'name': '沃尔沃',
+    'index': 'W',
+    'icon': '/images/che/99_90_2007106115753684.jpg'
+}, {
+    'name': '威兹曼',
+    'index': 'W',
+    'icon': '/images/che/99_90_200912011573379.jpg'
+}, {
+    'name': '威麟',
+    'index': 'W',
+    'icon': '/images/che/99_90_2009111115268312.jpg'
+}, {
+    'name': '五菱',
+    'index': 'W',
+    'icon': '/images/che/99_90_20101021121658419.jpg'
+}, {
+    'name': '新凯',
+    'index': 'X',
+    'icon': '/images/che/99_90_20130812041741748.jpg'
+}, {
+    'name': '现代',
+    'index': 'X',
+    'icon': '/images/che/99_90_2007106124214773.jpg'
+}, {
+    'name': '雪铁龙',
+    'index': 'X',
+    'icon': '/images/che/99_90_20130228035625768.jpg'
+}, {
+    'name': '西雅特',
+    'index': 'X',
+    'icon': '/images/che/99_90_200710611304786.jpg'
+}, {
+    'name': '雪佛兰',
+    'index': 'X',
+    'icon': '/images/che/99_90_2007106114045517.jpg'
+}, {
+    'name': '云度',
+    'index': 'Y',
+    'icon': '/images/che/99_90_20171011030718621.jpg'
+}, {
+    'name': '御捷',
+    'index': 'Y',
+    'icon': '/images/che/99_90_20171025100135497.jpg'
+}, {
+    'name': '驭胜',
+    'index': 'Y',
+    'icon': '/images/che/99_90_20160923044039737.jpg'
+}, {
+    'name': '游侠',
+    'index': 'Y',
+    'icon': '/images/che/99_90_20161221033228797.jpg'
+}, {
+    'name': '英菲尼迪',
+    'index': 'Y',
+    'icon': '/images/che/99_90_2007106125851705.jpg'
+}, {
+    'name': '依维柯',
+    'index': 'Y',
+    'icon': '/images/che/99_90_20068117321666557.gif'
+}, {
+    'name': '永源',
+    'index': 'Y',
+    'icon': '/images/che/99_90_201028155581.jpg'
+}, {
+    'name': '亚星',
+    'index': 'Z',
+    'icon': '/images/che/99_90_201028155581.jpg'
+}, {
+    'name': '知豆',
+    'index': 'Z',
+    'icon': '/images/che/99_90_20150629114603413.jpg'
+}, {
+    'name': '之诺',
+    'index': 'Z',
+    'icon': '/images/che/99_90_20161107030937550.jpg'
+}, {
+    'name': '中华',
+    'index': 'Z',
+    'icon': '/images/che/99_90_200710517734684.jpg'
+}, {
+    'name': '中兴',
+    'index': 'Z',
+    'icon': '/images/che/99_90_20068117125326880.gif'
+}, {
+    'name': '众泰',
+    'index': 'Z',
+    'icon': '/images/che/99_90_200911131072464.jpg'
+}, {
+    'name': '中通',
+    'index': 'Z',
+    'icon': '/images/che/99_90_200710522261046.jpg'
+}]
+export {
+    cheData
+}
